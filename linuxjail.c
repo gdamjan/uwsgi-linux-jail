1a4523e13a586399c6d3309ec36eb81700a1a6b3
Damjan Georgievski <gdamjan@gmail.com>
2014-07-18 16:09:03 +0200

    remount must use same flags as mount

--- linuxjail.c
+++ linuxjail.c
@@ -58,7 +58,7 @@ static void do_the_jail() {
     if (mount("/.orig_root/usr", "/usr", "none", MS_BIND, NULL) != 0) {
         uwsgi_fatal_error("mount(/usr)");
     }
-    if (mount(NULL, "/usr", NULL, MS_RDONLY | MS_REMOUNT, NULL) != 0) {
+    if (mount(NULL, "/usr", NULL, MS_BIND | MS_RDONLY | MS_REMOUNT, NULL) != 0) {
         uwsgi_fatal_error("remount(/usr)");
     }
 
